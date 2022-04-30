c13409cec53b808436ba6fad8fbbda6b8da4d6df
Cody Hatfield <cody.hatfield@me.com>
2022-04-30 16:03:48 -0700

    Generate DID document

---
+++ README.md
@@ -0,0 +1,25 @@
+## Setup
+
+See `.env.example` for needed environment variables.
+
+## Generate Seed
+
+```
+node scripts/generate-seed.js
+```
+
+Save this seed as `KEY_SEED`. This is the seed of the key pair used by the service.
+
+## Generate DID Document
+
+This command will generate the DID document to be published.
+
+```
+node scripts/generate-did-document.mjs > ./static/.well-known/did.json
+```
+
+## Publish DID Document
+
+```
+yarn moralis-admin-cli deploy -p static
+```
--- scripts/generate-did-document.mjs
+++ scripts/generate-did-document.mjs
@@ -5,9 +5,16 @@ import { Ed25519Provider } from "key-did-provider-ed25519";
 import { DID } from "dids";
 import KeyResolver from "key-did-resolver";
 
+const didDocTemplate = {
+  "@context": ["https://www.w3.org/ns/did/v1"],
+  id: `did:web:${process.env.SERVER_HOST}`,
+};
+
 const seed = fromString(process.env.KEY_SEED, "base16");
 const provider = new Ed25519Provider(seed);
 const did = new DID({ provider, resolver: KeyResolver.getResolver() });
 await did.authenticate();
 
-console.log(did.id);
+didDocTemplate["keyAgreement"] = [did.id];
+
+console.log(JSON.stringify(didDocTemplate, null, 2));
